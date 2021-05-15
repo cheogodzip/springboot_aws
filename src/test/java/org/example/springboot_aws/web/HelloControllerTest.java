ab8189a0f4c5da08b3af9bbf510812bbf4f61597
cheogodzip <lkver@naver.com>
2021-05-15 16:10:52 +0900

    스프링 부트 버전 변경, 의존성 버전 변경

--- src/test/java/org/example/springboot_aws/web/HelloControllerTest.java
+++ src/test/java/org/example/springboot_aws/web/HelloControllerTest.java
@@ -1,18 +1,17 @@
-package org.example.springboot_was.web;
+package org.example.springboot_aws.web;
 
-import org.example.springboot_aws.web.HelloController;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class) // JUnit이 아닌 다른 실행자를 실행. SpringRunner라는 스프링 실행자를 사용
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class) // Web 테스트에 집중할 수 있는 어노테이션. @Controller, @ControllerAdvice 등이 포함됨.
 public class HelloControllerTest {
 
