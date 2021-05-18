d1d49d210bd6328adbf12310a5424cf4cbc0581d
cheogodzip <lkver@naver.com>
2021-05-18 16:20:10 +0900

    머스티치 적용, index 페이지 추가

---
+++ src/main/java/org/example/springboot_aws/web/IndexController.java
@@ -0,0 +1,13 @@
+package org.example.springboot_aws.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
